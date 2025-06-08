bf3299ea2e1d864083953420181a18cc89fb619d
Matthew Casperson <matthewcasperson@gmail.com>
2025-06-09 08:38:53 +1000

    Fixed logic

--- octopusdeploy/config.go
+++ octopusdeploy/config.go
@@ -37,11 +37,12 @@ func (h *HeaderRoundTripper) RoundTrip(req *http.Request) (*http.Response, error
 }
 
 func getHttpClient(octopusUrl string) (*http.Client, error) {
-	if isDirectlyAccessibleOctopusInstance(octopusUrl) {
+	if !isDirectlyAccessibleOctopusInstance(octopusUrl) {
+		log.Printf("[INFO] Enabled Octopus AI Assistant redirection service")
 		return createHttpClient(octopusUrl)
 	}
 
-	log.Printf("[INFO] Enabled Octopus AI Assistant redirection service")
+	log.Printf("[INFO] Did not enable Octopus AI Assistant redirection service")
 
 	return nil, nil
 }
