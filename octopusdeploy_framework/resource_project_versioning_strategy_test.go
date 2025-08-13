3e41354d826b232bb59900ff795ac0b2535d6610
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-14 09:49:38 +1000

    Added a test

---
+++ octopusdeploy_framework/resource_project_versioning_strategy_test.go
@@ -0,0 +1,75 @@
+package octopusdeploy_framework
+
+import (
+	"fmt"
+	"testing"
+
+	internaltest "github.com/OctopusDeploy/terraform-provider-octopusdeploy/internal/test"
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestProjectVersioningStrategyWithUpdate(t *testing.T) {
+	lifecycleLocalName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	lifecycleName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	projectGroupLocalName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	projectGroupName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	localName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	versioningStrategyPrefix := "octopusdeploy_project_versioning_strategy." + localName
+	template := "#{Octopus.Version.LastMajor}.#{Octopus.Version.LastMinor}.#{Octopus.Version.LastPatch}.#{Octopus.Version.NextRevision}"
+	template2 := "#{Octopus.Date.Year}.#{Octopus.Date.Month}.#{Octopus.Date.Day}.i"
+
+	description := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	name := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+
+	resource.Test(t, resource.TestCase{
+		CheckDestroy: resource.ComposeTestCheckFunc(
+			testAccProjectCheckDestroy,
+			testAccLifecycleCheckDestroy,
+		),
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccProjectCheckExists(),
+					resource.TestCheckResourceAttr(versioningStrategyPrefix, "template", template),
+				),
+				Config: testProjectVersioningStrategy(lifecycleLocalName, lifecycleName, projectGroupLocalName, projectGroupName, localName, name, description, template),
+			},
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccProjectCheckExists(),
+					resource.TestCheckResourceAttr(versioningStrategyPrefix, "template", template2),
+				),
+				Config: testProjectVersioningStrategy(lifecycleLocalName, lifecycleName, projectGroupLocalName, projectGroupName, localName, name, description, template2),
+			},
+		},
+	})
+}
+
+func testProjectVersioningStrategy(lifecycleLocalName string, lifecycleName string, projectGroupLocalName string, projectGroupName string, localName string, name string, description string, template string) string {
+	projectGroup := internaltest.NewProjectGroupTestOptions()
+	projectGroup.LocalName = projectGroupLocalName
+	projectGroup.Resource.Name = projectGroupName
+
+	return fmt.Sprintf(testAccLifecycle(lifecycleLocalName, lifecycleName)+"\n"+
+		internaltest.ProjectGroupConfiguration(projectGroup)+"\n"+
+		`resource "octopusdeploy_project" "%s" {
+			description      = "%s"
+			lifecycle_id     = octopusdeploy_lifecycle.%s.id
+			name             = "%s"
+			project_group_id = octopusdeploy_project_group.%s.id
+
+			connectivity_policy {
+				allow_deployments_to_no_targets = true
+				skip_machine_behavior           = "None"
+			}
+
+		}
+
+		resource "octopusdeploy_project_versioning_strategy" "%s" {
+		  project_id = "${octopusdeploy_project.%s.id}"
+		  template   = "%s"
+		}`, localName, description, lifecycleLocalName, name, projectGroupLocalName, localName, localName, template)
+}
