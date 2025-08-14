dc874184d7583f8dc74aff8d93eb1d09444b212c
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-14 10:10:38 +1000

    Added a test with a package reference

--- octopusdeploy_framework/resource_project_versioning_strategy_test.go
+++ octopusdeploy_framework/resource_project_versioning_strategy_test.go
@@ -44,6 +44,14 @@ func TestProjectVersioningStrategyWithUpdate(t *testing.T) {
 				),
 				Config: testProjectVersioningStrategy(lifecycleLocalName, lifecycleName, projectGroupLocalName, projectGroupName, localName, name, description, template2),
 			},
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccProjectCheckExists(),
+					resource.TestCheckResourceAttr(versioningStrategyPrefix, "donor_package.deployment_action", "Hello World"),
+					resource.TestCheckResourceAttr(versioningStrategyPrefix, "donor_package.package_reference", ""),
+				),
+				Config: testProjectVersioningStrategyFromPackage(lifecycleLocalName, lifecycleName, projectGroupLocalName, projectGroupName, localName, name, description),
+			},
 		},
 	})
 }
@@ -65,11 +73,130 @@ func testProjectVersioningStrategy(lifecycleLocalName string, lifecycleName stri
 				allow_deployments_to_no_targets = true
 				skip_machine_behavior           = "None"
 			}
+		}
 
+		resource "octopusdeploy_process" "%s" {
+		  project_id  = octopusdeploy_project.%s.id
+		}
+
+		resource "octopusdeploy_process_step" "%s" {
+		  process_id  = octopusdeploy_process.%s.id
+		  name = "%s"
+		  properties = {
+			"Octopus.Action.TargetRoles" = "role-one"
+		  }
+		  type = "Octopus.TentaclePackage"
+		  primary_package = {
+			package_id: "my.package"
+			feed_id: "built-in"
+		  }
+		  execution_properties = {
+			"Octopus.Action.RunOnServer" = "True"    
+		  }
+		}
+
+		resource octopusdeploy_process_steps_order "%s" {
+		  process_id = octopusdeploy_process.%s.id
+		  steps = [
+			octopusdeploy_process_step.%s.id
+		  ]
 		}
 
 		resource "octopusdeploy_project_versioning_strategy" "%s" {
 		  project_id = "${octopusdeploy_project.%s.id}"
 		  template   = "%s"
-		}`, localName, description, lifecycleLocalName, name, projectGroupLocalName, localName, localName, template)
+		}`,
+		localName,
+		description,
+		lifecycleLocalName,
+		name,
+		projectGroupLocalName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		template)
+}
+
+func testProjectVersioningStrategyFromPackage(lifecycleLocalName string, lifecycleName string, projectGroupLocalName string, projectGroupName string, localName string, name string, description string) string {
+	projectGroup := internaltest.NewProjectGroupTestOptions()
+	projectGroup.LocalName = projectGroupLocalName
+	projectGroup.Resource.Name = projectGroupName
+
+	configuration := fmt.Sprintf(testAccLifecycle(lifecycleLocalName, lifecycleName)+"\n"+
+		internaltest.ProjectGroupConfiguration(projectGroup)+"\n"+
+		`resource "octopusdeploy_project" "%s" {
+			description      = "%s"
+			lifecycle_id     = octopusdeploy_lifecycle.%s.id
+			name             = "%s"
+			project_group_id = octopusdeploy_project_group.%s.id
+
+			connectivity_policy {
+				allow_deployments_to_no_targets = true
+				skip_machine_behavior           = "None"
+			}
+		}
+
+		resource "octopusdeploy_process" "%s" {
+		  project_id  = octopusdeploy_project.%s.id
+		}
+
+		resource "octopusdeploy_process_step" "%s" {
+		  process_id  = octopusdeploy_process.%s.id
+		  name = "Hello World"
+		  properties = {
+			"Octopus.Action.TargetRoles" = "role-one"
+		  }
+		  type = "Octopus.TentaclePackage"
+		  primary_package = {
+			package_id: "Package"
+			feed_id: "built-in"
+		  }
+		  execution_properties = {
+			"Octopus.Action.RunOnServer" = "True"    
+		  }
+		}
+
+		resource octopusdeploy_process_steps_order "%s" {
+		  process_id = octopusdeploy_process.%s.id
+		  steps = [
+			octopusdeploy_process_step.%s.id
+		  ]
+		}
+
+		resource "octopusdeploy_project_versioning_strategy" "%s" {
+		  project_id = "${octopusdeploy_project.%s.id}"
+		  donor_package = {
+			deployment_action = "Hello World"
+			package_reference = ""
+		  }
+          depends_on = [
+			octopusdeploy_process_step.%s,
+			octopusdeploy_process.%s
+		  ]
+		}`,
+		localName,
+		description,
+		lifecycleLocalName,
+		name,
+		projectGroupLocalName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName)
+
+	return configuration
 }
