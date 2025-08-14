5a6b60f209c0a4fbba87937a4fd2d2f998a51c8e
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-14 10:20:36 +1000

    Added a test with a donor package

--- octopusdeploy_framework/resource_project_versioning_strategy_test.go
+++ octopusdeploy_framework/resource_project_versioning_strategy_test.go
@@ -52,6 +52,18 @@ func TestProjectVersioningStrategyWithUpdate(t *testing.T) {
 				),
 				Config: testProjectVersioningStrategyFromPackage(lifecycleLocalName, lifecycleName, projectGroupLocalName, projectGroupName, localName, name, description),
 			},
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccProjectCheckExists(),
+					resource.TestCheckResourceAttrWith(versioningStrategyPrefix, "donor_package_step_id", func(value string) error {
+						if value == "" {
+							return fmt.Errorf("donor_package_step_id should not be empty")
+						}
+						return nil
+					}),
+				),
+				Config: testProjectVersioningStrategyFromStep(lifecycleLocalName, lifecycleName, projectGroupLocalName, projectGroupName, localName, name, description),
+			},
 		},
 	})
 }
@@ -186,6 +198,108 @@ func testProjectVersioningStrategyFromPackage(lifecycleLocalName string, lifecyc
 		lifecycleLocalName,
 		name,
 		projectGroupLocalName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName)
+
+	return configuration
+}
+
+func testProjectVersioningStrategyFromStep(lifecycleLocalName string, lifecycleName string, projectGroupLocalName string, projectGroupName string, localName string, name string, description string) string {
+	projectGroup := internaltest.NewProjectGroupTestOptions()
+	projectGroup.LocalName = projectGroupLocalName
+	projectGroup.Resource.Name = projectGroupName
+
+	configuration := fmt.Sprintf(testAccLifecycle(lifecycleLocalName, lifecycleName)+"\n"+
+		internaltest.ProjectGroupConfiguration(projectGroup)+"\n"+
+		`resource "octopusdeploy_project" "%s" {
+			description      = "%s"
+			lifecycle_id     = octopusdeploy_lifecycle.%s.id
+			name             = "%s"
+			project_group_id = octopusdeploy_project_group.%s.id
+
+			connectivity_policy {
+				allow_deployments_to_no_targets = true
+				skip_machine_behavior           = "None"
+			}
+		}
+
+		resource "octopusdeploy_process" "%s" {
+		  project_id  = octopusdeploy_project.%s.id
+		}
+
+		resource "octopusdeploy_process_step" "%s" {
+		  process_id  = octopusdeploy_process.%s.id
+		  name = "Hello World"
+		  properties = {
+			"Octopus.Action.TargetRoles" = "role-one"
+		  }
+		  type = "Octopus.TentaclePackage"
+		  primary_package = {
+			package_id: "Package"
+			feed_id: "built-in"
+		  }
+		  execution_properties = {
+			"Octopus.Action.RunOnServer" = "True"    
+		  }
+		}
+
+		resource octopusdeploy_process_steps_order "%s" {
+		  process_id = octopusdeploy_process.%s.id
+		  steps = [
+			octopusdeploy_process_step.%s.id
+		  ]
+		}
+
+		resource "octopusdeploy_process_child_step" "%s" {
+		  process_id  = octopusdeploy_process.%s.id
+		  parent_id = octopusdeploy_process_step.%s.id
+		  name = "Hello World Child"
+		  type = "Octopus.TentaclePackage"
+		  primary_package = {
+			package_id: "Package"
+			feed_id: "built-in"
+		  }
+		  execution_properties = {
+			"Octopus.Action.RunOnServer" = "True"    
+		  }
+		}
+
+		resource "octopusdeploy_process_child_steps_order" "%s" {
+		  process_id = octopusdeploy_process.%s.id
+		  parent_id = octopusdeploy_process_step.%s.id
+		  children = [
+			octopusdeploy_process_child_step.%s.id,
+		  ]
+		}
+
+		resource "octopusdeploy_project_versioning_strategy" "%s" {
+		  project_id = "${octopusdeploy_project.%s.id}"
+		  donor_package_step_id = octopusdeploy_process_child_step.%s.id
+          depends_on = [
+			octopusdeploy_process.%s
+		  ]
+		}`,
+		localName,
+		description,
+		lifecycleLocalName,
+		name,
+		projectGroupLocalName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
+		localName,
 		localName,
 		localName,
 		localName,
