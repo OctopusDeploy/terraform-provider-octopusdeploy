ed29f18eab1f4016fe690df6d5393e774ce69ed7
Rose Northey <rose.northey@octopus.com>
2025-08-19 16:25:15 +1200

    test default retention policy validator when adding unwanted attributes

--- octopusdeploy_framework/resource_lifecycle_test.go
+++ octopusdeploy_framework/resource_lifecycle_test.go
@@ -395,6 +395,43 @@ func TestAccLifecycleComplex(t *testing.T) {
 	})
 }
 
+func TestAccLifecycleDefaultReleaseRetentionValidation(t *testing.T) {
+	localName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	name := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	shouldKeepForeverAttribute := "should_keep_forever = false"
+	quantityToKeepAttribute := "quantity_to_keep = 0"
+	unitAttribute := `unit = "Items"`
+
+	resource.Test(t, resource.TestCase{
+		CheckDestroy:             testAccLifecycleCheckDestroy,
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			//should error when should_keep_forever is also supplied
+			{
+				Config:   createLifecycleWithDefaultReleaseRetentionAndUnwantedAttribute(localName, name, quantityToKeepAttribute),
+				PlanOnly: true,
+				ExpectError: regexp.MustCompile(
+					`quantity_to_keep should not be supplied when strategy is set to Default`,
+				),
+			},
+			{
+				Config:   createLifecycleWithDefaultReleaseRetentionAndUnwantedAttribute(localName, name, shouldKeepForeverAttribute),
+				PlanOnly: true,
+				ExpectError: regexp.MustCompile(
+					"should_keep_forever should not be supplied when strategy is set to Default",
+				),
+			},
+			{
+				Config:   createLifecycleWithDefaultReleaseRetentionAndUnwantedAttribute(localName, name, unitAttribute),
+				PlanOnly: true,
+				ExpectError: regexp.MustCompile(
+					`unit should not be supplied when strategy is set to Default`,
+				),
+			},
+		},
+	})
+}
 func testAccLifecycle(localName string, name string) string {
 	return fmt.Sprintf(`resource "octopusdeploy_lifecycle" "%s" {
 		name = "%s"
