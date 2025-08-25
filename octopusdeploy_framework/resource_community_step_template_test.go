b125cfba6131be52e9ed3f3cec022293f0245639
Matthew Casperson <matthewcasperson@gmail.com>
2025-08-25 15:58:09 +1000

    Force a community step template refresh and wait for the task to complete

--- octopusdeploy_framework/resource_community_step_template_test.go
+++ octopusdeploy_framework/resource_community_step_template_test.go
@@ -1,11 +1,15 @@
 package octopusdeploy_framework
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/actiontemplates"
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/tasks"
 	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/hashicorp/terraform-plugin-testing/terraform"
@@ -67,7 +71,7 @@ func forceCommunityStepTemplateRefresh() {
 		return
 	}
 
-	_, err := octoClient.
+	req, err := octoClient.
 		Sling().
 		Post("/api/tasks").
 		BodyJSON(task{
@@ -78,6 +82,45 @@ func forceCommunityStepTemplateRefresh() {
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error forcing community step template refresh")
+	}
+
+	resp, err := octoClient.Sling().Do(req, nil, nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error executing community step template refresh: %v\n", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading response body: %v\n", err)
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "Community step template refresh response: %s\n", string(body))
+
+	task := tasks.Task{}
+	if err := json.Unmarshal(body, &task); err != nil {
+		fmt.Fprintf(os.Stderr, "Error unmarshaling response body: %v\n", err)
+		return
+	}
+
+	fmt.Println(task.ID)
+
+	// Poll the task status until it is completed
+	for i := 0; i < 24; i++ {
+		newTask, err := tasks.GetDetails(octoClient, octoClient.GetSpaceID(), task.ID)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error getting task details: %v\n", err)
+			return
+		}
+
+		if newTask.Task.State == "Success" || newTask.Task.State == "Failed" || newTask.Task.State == "Canceled" {
+			fmt.Fprintf(os.Stderr, "Community step template refresh completed\n")
+			return
+		}
+
+		time.Sleep(5 * time.Second)
 	}
 }
 
