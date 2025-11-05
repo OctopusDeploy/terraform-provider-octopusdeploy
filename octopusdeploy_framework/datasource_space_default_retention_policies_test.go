29bf1c148c7b658872af61a9d56629bfaa5d96da
Hideaki Murakami <hideaki.murakami@octopus.com>
2025-11-05 16:01:11 +1300

    Create datasource test

---
+++ octopusdeploy_framework/datasource_space_default_retention_policies_test.go
@@ -0,0 +1,49 @@
+package octopusdeploy_framework
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDataSourceSpaceDefaultRetentionPolicy(t *testing.T) {
+	spaceName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	retentionType := "LifecycleRelease"
+	resourceName := "data.octopusdeploy_space_default_retention_policy.space_default_retention_policy_test"
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories(),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceSpaceDefaultRetentionPolicyConfig(spaceName, retentionType),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(resourceName, "retention_policy.id"),
+					resource.TestCheckResourceAttrSet(resourceName, "retention_policy.space_id"),
+					resource.TestCheckResourceAttr(resourceName, "retention_policy.retention_type", retentionType),
+					resource.TestCheckResourceAttrSet(resourceName, "retention_policy.strategy"),
+				),
+			},
+		},
+	})
+}
+
+func testAccDataSourceSpaceDefaultRetentionPolicyConfig(spaceName, retention_type string) string {
+	return fmt.Sprintf(`
+resource "octopusdeploy_space" "octopus_project_space_test" {
+  name                  = "%s"
+  is_default            = false
+  is_task_queue_stopped = false
+  description           = "Test space for lifecycles datasource"
+  space_managers_teams  = ["teams-administrators"]
+}
+
+data "octopusdeploy_space_default_retention_policy" "space_default_retention_policy_test" {
+  space_id       = octopusdeploy_space.octopus_project_space_test.id
+  retention_type = "%s"
+}
+`, spaceName, retention_type)
+}
