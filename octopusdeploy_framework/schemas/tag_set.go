55ec553895a64621400c16ef375d5fc614d85b95
Huy Nguyen <162080607+HuyPhanNguyen@users.noreply.github.com>
2026-03-02 11:16:35 +1000

    feat: Add Runbook Tags support to runbook resource (#152)
    * feat: add runbook_tags support to runbook resource
    
    * update go-octopusdeploy version
    
    * remove compatibility check cant not apply for resource property

--- octopusdeploy_framework/schemas/tag_set.go
+++ octopusdeploy_framework/schemas/tag_set.go
@@ -18,6 +18,7 @@ var tagSetScopes = []string{
 	"Tenant",
 	"Environment",
 	"Project",
+	"Runbook",
 	"Target",
 }
 
@@ -48,7 +49,7 @@ func (t TagSetSchema) GetResourceSchema() resourceSchema.Schema {
 			"scopes": util.ResourceList(types.StringType).
 				Optional().
 				Computed().
-				Description("The resource scopes this tag set applies to. Valid values are `\"Tenant\"`, `\"Environment\"`, `\"Project\"`, `\"Target\"`.").
+				Description("The resource scopes this tag set applies to. Valid values are `\"Tenant\"`, `\"Environment\"`, `\"Project\"`, `\"Runbook\"`, `\"Target\"`.").
 				Validators(listvalidator.ValueStringsAre(stringvalidator.OneOf(tagSetScopes...))).
 				Build(),
 			"sort_order": util.ResourceInt64().
@@ -94,7 +95,7 @@ func (t TagSetSchema) GetDatasourceSchema() datasourceSchema.Schema {
 				Build(),
 			"scopes": util.DataSourceList(types.StringType).
 				Optional().
-				Description("A filter to search by scopes. Valid values are `\"Tenant\"`, `\"Environment\"`, `\"Project\"`, `\"Target\"`.").
+				Description("A filter to search by scopes. Valid values are `\"Tenant\"`, `\"Environment\"`, `\"Project\"`, `\"Runbook\"`, `\"Target\"`.").
 				Build(),
 			"skip": util.DataSourceInt64().
 				Optional().
@@ -124,7 +125,7 @@ func (t TagSetSchema) GetDatasourceSchema() datasourceSchema.Schema {
 							Build(),
 						"scopes": util.DataSourceList(types.StringType).
 							Computed().
-							Description("The resource scopes this tag set applies to. Valid values are `\"Tenant\"`, `\"Environment\"`, `\"Project\"`, `\"Target\"`.").
+							Description("The resource scopes this tag set applies to. Valid values are `\"Tenant\"`, `\"Environment\"`, `\"Project\"`, `\"Runbook\"`, `\"Target\"`.").
 							Build(),
 						"sort_order": util.DataSourceInt64().
 							Computed().
