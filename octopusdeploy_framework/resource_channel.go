e42c14cfed528e9a9b6fe67d5db073e3eac6a53e
CaitlynStocker <caitlyn.stocker@octopus.com>
2026-04-02 16:36:19 +1000

    Add custom fields to channel entity

--- octopusdeploy_framework/resource_channel.go
+++ octopusdeploy_framework/resource_channel.go
@@ -139,6 +139,7 @@ func expandChannel(ctx context.Context, model schemas.ChannelModel) *channels.Ch
 	channel.Description = model.Description.ValueString()
 	channel.IsDefault = model.IsDefault.ValueBool()
 	channel.LifecycleID = model.LifecycleId.ValueString()
+	channel.CustomFieldDefinitions = expandChannelCustomFieldDefinitions(model.CustomFieldDefinitions)
 	channel.Rules = expandChannelRules(model.Rule)
 	channel.SpaceID = model.SpaceId.ValueString()
 	channel.TenantTags = util.ExpandStringSet(model.TenantTags)
@@ -252,6 +253,7 @@ func flattenChannel(ctx context.Context, channel *channels.Channel, model schema
 	model.Name = types.StringValue(channel.Name)
 	model.ProjectId = types.StringValue(channel.ProjectID)
 
+	model.CustomFieldDefinitions = flattenChannelCustomFieldDefinitions(channel.CustomFieldDefinitions)
 	model.Rule = flattenChannelRules(channel.Rules, model.Rule)
 
 	if channel.SpaceID == "" && model.SpaceId.IsNull() {
@@ -331,5 +333,49 @@ func getChannelRuleDeploymentActionPackageAttrTypes() map[string]attr.Type {
 	return map[string]attr.Type{
 		"deployment_action": types.StringType,
 		"package_reference": types.StringType,
+	}
+}
+
+func expandChannelCustomFieldDefinitions(defs types.List) []channels.ChannelCustomFieldDefinition {
+	if defs.IsNull() || defs.IsUnknown() || len(defs.Elements()) == 0 {
+		return []channels.ChannelCustomFieldDefinition{}
+	}
+
+	result := make([]channels.ChannelCustomFieldDefinition, 0, len(defs.Elements()))
+	for _, elem := range defs.Elements() {
+		obj := elem.(types.Object)
+		attrs := obj.Attributes()
+
+		var def channels.ChannelCustomFieldDefinition
+		if v, ok := attrs["field_name"].(types.String); ok && !v.IsNull() {
+			def.FieldName = v.ValueString()
+		}
+		if v, ok := attrs["description"].(types.String); ok && !v.IsNull() {
+			def.Description = v.ValueString()
+		}
+		result = append(result, def)
+	}
+	return result
+}
+
+func flattenChannelCustomFieldDefinitions(defs []channels.ChannelCustomFieldDefinition) types.List {
+	if len(defs) == 0 {
+		return types.ListNull(types.ObjectType{AttrTypes: getChannelCustomFieldDefinitionAttrTypes()})
+	}
+
+	elems := make([]attr.Value, 0, len(defs))
+	for _, def := range defs {
+		elems = append(elems, types.ObjectValueMust(getChannelCustomFieldDefinitionAttrTypes(), map[string]attr.Value{
+			"field_name":  types.StringValue(def.FieldName),
+			"description": types.StringValue(def.Description),
+		}))
+	}
+	return types.ListValueMust(types.ObjectType{AttrTypes: getChannelCustomFieldDefinitionAttrTypes()}, elems)
+}
+
+func getChannelCustomFieldDefinitionAttrTypes() map[string]attr.Type {
+	return map[string]attr.Type{
+		"field_name":  types.StringType,
+		"description": types.StringType,
 	}
 }
