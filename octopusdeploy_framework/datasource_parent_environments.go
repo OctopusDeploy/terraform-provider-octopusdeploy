07a70e7e15fbcc0e3c0b7c385ce57f8f587cb1a0
Matthew Casperson <matthewcasperson@gmail.com>
2026-06-29 09:50:27 +1000

    Add data source for parent environments (#226)
    * Added a data source for parent environments
    
    * feat: Add documentation for octopusdeploy_parent_environments data source
    
    * feat: Add Type field to parent environments data source
    
    * feat: Update parent environments data source to include ParentEnvironments field and adjust schema
    
    * fix: Correct attribute name in parent environments data source test
    
    * feat: Add automatic deprovisioning rule to parent environments in tests
    
    * feat: Add additional space manager team and limit results in parent environments data source test
    
    * feat: Prioritize Name over PartialName in environment query

---
+++ octopusdeploy_framework/datasource_parent_environments.go
@@ -0,0 +1,103 @@
+package octopusdeploy_framework
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/OctopusDeploy/go-octopusdeploy/v2/pkg/environments/v2/environments"
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/schemas"
+	"github.com/OctopusDeploy/terraform-provider-octopusdeploy/octopusdeploy_framework/util"
+	"github.com/hashicorp/terraform-plugin-framework/datasource"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-log/tflog"
+)
+
+type parentEnvironmentDataSource struct {
+	*Config
+}
+
+type parentEnvironmentsDataSourceModel struct {
+	ID                 types.String `tfsdk:"id"`
+	SpaceID            types.String `tfsdk:"space_id"`
+	IDs                types.List   `tfsdk:"ids"`
+	PartialName        types.String `tfsdk:"partial_name"`
+	Name               types.String `tfsdk:"name"`
+	Skip               types.Int64  `tfsdk:"skip"`
+	Take               types.Int64  `tfsdk:"take"`
+	ParentEnvironments types.List   `tfsdk:"parent_environments"`
+}
+
+func NewParentEnvironmentsDataSource() datasource.DataSource {
+	return &parentEnvironmentDataSource{}
+}
+
+func (*parentEnvironmentDataSource) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
+	resp.TypeName = util.GetTypeName("parent_environments")
+}
+
+func (*parentEnvironmentDataSource) Schema(_ context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
+	resp.Schema = schemas.ParentEnvironmentSchema{}.GetDatasourceSchema()
+}
+
+func (e *parentEnvironmentDataSource) Configure(_ context.Context, req datasource.ConfigureRequest, resp *datasource.ConfigureResponse) {
+	e.Config = DataSourceConfiguration(req, resp)
+}
+
+func (e *parentEnvironmentDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+	var err error
+	var data parentEnvironmentsDataSourceModel
+	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Name takes precedence over PartialName
+	partialName := data.PartialName.ValueString()
+	if !data.Name.IsNull() {
+		partialName = data.Name.ValueString()
+	}
+
+	query := environments.EnvironmentQuery{
+		IDs:         util.GetIds(data.IDs),
+		PartialName: partialName,
+		Skip:        util.GetNumber(data.Skip),
+		Take:        util.GetNumber(data.Take),
+		Type:        []string{"Parent"},
+	}
+
+	util.DatasourceReading(ctx, "parent_environments", query)
+
+	existingEnvironments, err := environments.Get(e.Client, data.SpaceID.ValueString(), query)
+	if err != nil {
+		resp.Diagnostics.AddError("unable to load parent environments", err.Error())
+		return
+	}
+
+	mappedEnvironments := []schemas.ParentEnvironmentDatasourceModel{}
+	if data.Name.IsNull() {
+		tflog.Debug(ctx, fmt.Sprintf("parent environments returned from API: %#v", existingEnvironments))
+		for _, environment := range existingEnvironments.Items {
+			mappedEnvironments = append(mappedEnvironments, schemas.MapFromParentEnvironmentForDatasource(ctx, environment))
+		}
+	} else { // if name has been specified, match by exact name rather than partial name as the API does
+		var matchedEnvironment *environments.Environment
+		tflog.Debug(ctx, fmt.Sprintf("matching parent environment by name: %s", data.Name))
+		for _, env := range existingEnvironments.Items {
+			if strings.EqualFold(env.Name, data.Name.ValueString()) {
+				matchedEnvironment = env
+			}
+		}
+		if matchedEnvironment != nil {
+			mappedEnvironments = append(mappedEnvironments, schemas.MapFromParentEnvironmentForDatasource(ctx, matchedEnvironment))
+		}
+	}
+
+	util.DatasourceResultCount(ctx, "parent_environments", len(mappedEnvironments))
+
+	data.ParentEnvironments, _ = types.ListValueFrom(ctx, types.ObjectType{AttrTypes: schemas.ParentEnvironmentDatasourceObjectType()}, mappedEnvironments)
+	data.ID = types.StringValue("Parent Environments " + time.Now().UTC().String())
+
+	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
+}
---
+++ octopusdeploy_framework/parent_datasource_environments_test.go
@@ -0,0 +1,138 @@
+package octopusdeploy_framework
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/acctest"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+)
+
+func TestAccDataSourceParentEnvironments(t *testing.T) {
+	localName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	prefix := fmt.Sprintf("data.octopusdeploy_parent_environments.%s", localName)
+
+	spaceName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+
+	environmentLocalName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+	environmentName := acctest.RandStringFromCharSet(20, acctest.CharSetAlpha)
+
+	take := 10
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: ProtoV6ProviderFactories(),
+		PreCheck:                 func() { TestAccPreCheck(t) },
+		Steps: []resource.TestStep{
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckParentEnvironmentsDataSourceID(prefix),
+				),
+				Config: testAccDataSourceParentEnvironmentsEmpty(localName),
+			},
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckParentEnvironmentsDataSourceID(prefix),
+					resource.TestCheckResourceAttr(prefix, "parent_environments.#", "3"),
+				),
+				Config: fmt.Sprintf(`%s
+				
+				%s`,
+					createTestAccDataSourceParentEnvironmentsConfig(spaceName, environmentLocalName, environmentName),
+					testAccDataSourceParentEnvironmentsConfig(localName, take, spaceName, environmentLocalName),
+				),
+			},
+			{
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckParentEnvironmentsDataSourceID(prefix),
+					resource.TestCheckResourceAttr(prefix, "name", environmentName),
+					resource.TestCheckResourceAttr(prefix, "parent_environments.#", "1"),
+					resource.TestCheckResourceAttrSet(prefix, "parent_environments.0.id"),
+					resource.TestCheckResourceAttr(prefix, "parent_environments.0.name", environmentName),
+				),
+				Config: fmt.Sprintf(`%s
+
+			%s`,
+					createTestAccDataSourceParentEnvironmentsConfig(spaceName, environmentLocalName, environmentName),
+					testAccDataSourceParentEnvironmentByNameConfig(localName, environmentName, spaceName, environmentLocalName),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckParentEnvironmentsDataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		all := s.RootModule().Resources
+		rs, ok := all[n]
+		if !ok {
+			return fmt.Errorf("cannot find Environments data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("snapshot Environments source ID not set")
+		}
+		return nil
+	}
+}
+
+func createTestAccDataSourceParentEnvironmentsConfig(spaceName string, localName string, name string) string {
+	return fmt.Sprintf(`
+		resource "octopusdeploy_space" "%[1]s" {
+			name                  = "%[1]s"
+			is_default            = false
+			is_task_queue_stopped = true
+			description           = "Test space for environments datasource"
+			space_managers_teams  = ["teams-administrators", "teams-managers"]
+		}
+
+		resource "octopusdeploy_parent_environment" "%[2]s" {
+			name = "%[3]s"
+			space_id = octopusdeploy_space.%[1]s.id
+			automatic_deprovisioning_rule = {
+			  days  = 7 
+			  hours = 0 
+			}
+		}
+		
+		resource "octopusdeploy_parent_environment" "%[2]s-1" {
+			name = "%[3]s-1"
+			space_id = octopusdeploy_space.%[1]s.id
+			automatic_deprovisioning_rule = {
+			  days  = 7 
+			  hours = 0 
+			}
+		}
+
+		resource "octopusdeploy_parent_environment" "%[2]s-2" {
+			name = "%[3]s-2"
+			space_id = octopusdeploy_space.%[1]s.id
+			automatic_deprovisioning_rule = {
+			  days  = 7 
+			  hours = 0 
+			}
+		}
+	`, spaceName, localName, name)
+}
+
+func testAccDataSourceParentEnvironmentsEmpty(localName string) string {
+	return fmt.Sprintf(`data "octopusdeploy_parent_environments" "%s" {}`, localName)
+}
+
+func testAccDataSourceParentEnvironmentsConfig(localName string, take int, spaceName string, environmentLocalName string) string {
+	return fmt.Sprintf(`data "octopusdeploy_parent_environments" "%s" {
+		take = %v
+		space_id = octopusdeploy_space.%s.id
+		depends_on = [octopusdeploy_parent_environment.%s, octopusdeploy_parent_environment.%[4]s-1, octopusdeploy_parent_environment.%[4]s-2]
+	}`, localName, take, spaceName, environmentLocalName)
+}
+
+func testAccDataSourceParentEnvironmentByNameConfig(localName string, name string, spaceName string, environmentLocalName string) string {
+	return fmt.Sprintf(`data "octopusdeploy_parent_environments" "%s" {
+		name = "%s"	
+		take = 10
+		space_id = octopusdeploy_space.%s.id
+		depends_on = [octopusdeploy_parent_environment.%s, octopusdeploy_parent_environment.%[4]s-1, octopusdeploy_parent_environment.%[4]s-2]
+	}`, localName, name, spaceName, environmentLocalName)
+}
